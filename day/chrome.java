eba7186f9433c3a89fccf227a856d3b4a8dcce81
GayuSanjeev <gayathri280202@gmail.com>
2025-04-27 11:18:20 +0530

    Add files via upload
    day1

---
+++ day/chrome.java
@@ -0,0 +1,22 @@
+package week1.day;
+
+public class chrome {
+	public static void main(String args[])
+	{
+	String name="Gayathri";
+	System.out.println(name);
+	
+	float version=91.0f;
+	System.out.println(version);
+	
+	boolean isBeta=false;
+	System.out.println(isBeta);
+	
+	int release=2007;
+	System.out.println(release);
+	
+	char shortcutkey='v';
+	System.out.println(shortcutkey);
+	}
+
+}
